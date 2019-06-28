7934e2c4b2ac46c4b4360e12a86edc91c657b56e
Anele Mhlanga <aemhlanga@gmail.com>
2019-06-28 09:58:03 +0200

    Import R.class for AddNoteView

--- app/src/main/java/com/anelemhlanga/room/Views/AddNoteView.java
+++ app/src/main/java/com/anelemhlanga/room/Views/AddNoteView.java
@@ -3,6 +3,8 @@ package com.anelemhlanga.room.Views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anelemhlanga.room.R;
+
 public class AddNoteView extends AppCompatActivity {
 
     @Override
