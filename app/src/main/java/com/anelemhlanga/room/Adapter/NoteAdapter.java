ade9c5581d25a92f82746f69331362255b7b05cb
Anele Mhlanga Sr <aemhlanga@gmail.com>
2019-06-28 09:50:32 +0200

    Delete NoteAdapter

--- app/src/main/java/com/anelemhlanga/room/Adapter/NoteAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.anelemhlanga.room.Adapter;
-
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.ViewGroup;
-
-public class NoteAdapter extends RecyclerView.Adapter {
-    
-}
